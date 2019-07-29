e8fab796e822d614dbf9578e1f1aa7b6c39d77b1
zhuyinghui <404993465@qq.com>
2019-07-29 18:03:12 +0800

    11

--- public/mongodb.js
+++ public/mongodb.js
@@ -4,67 +4,42 @@ const mongoose = require("mongoose");
 //String/Number/Date/Buffer/Boolean/Mixed/ObjectId/Array
 let Model;
 class Mongodb {
-    // constructor(x, y) {
-    //     this.x = x;
-    //     this.y = y;
-    // }
     connect(){
         mongoose.connect("mongodb://127.0.0.1:27017/LifeRecord",{ useNewUrlParser: true })
             .then(
                 ()=>{
                     console.log('数据库连接成功');
-                    this.createTable();
                 },
                 err=>{
                     console.log('数据库连接失败',err);
                 }
             )
     }
-    createTable(){
-        //参数1，文档的字段及数据类型，对象
-        let schema={
-            userName:String,
-            userPassword:String
-        };
-        //参数2，数据库表名的单数形式，字符串
-        let tableName='user';
-        //参数3，要添加的数据，对象
-        const Schema=mongoose.Schema(schema);
-        Model=mongoose.model(tableName,Schema);
-        console.log('表格创建完成')
-    }
-    addOne(){
-        //传入参数
-        //参数1，文档的字段及数据类型，对象
-        // let schema={
-        //     userName:String,
-        //     userPassword:String
-        // };
-        //参数2，数据库表名的单数形式，字符串
-        // let tableName='user';
-        //参数3，要添加的数据，对象
-        // let data={
-        //     userName:'张三',
-        //     userPassword:123456
-        // };
-        let data={
-            userName:'李四',
-            userPassword:123456
-        };
-        const doc=new Model(data);
-        const promise=new Promise((resolve,reject)=>{
-            console.log('promise')
-            console.log(doc)
-            doc.save(err=>{
-                if(err){
-                    reject(err);
-                }else{
-                    resolve('数据添加成功')
-                }
+    usersModel(){  //用户表
+        if(!Model){
+            const Schema=mongoose.Schema({
+                userName:String,
+                userPassword:String
             });
-        });
-        return promise;
+            Model=mongoose.model('user',Schema);
+            return Model;
+        }else{
+            return Model
+        }
     }
+    othersModel(){
+        if(!Model){
+            const Schema=mongoose.Schema({
+                userName:String,
+                userPassword:String
+            });
+            Model=mongoose.model('other',Schema);
+            return Model;
+        }else{
+            return Model
+        }
+    }
+    
 }
 let db=new Mongodb();
 module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,44 @@ router.get('/', async (ctx, next) => {
     title: 'Hello Koa 2!'
   })
 });
-router.get('/addOne',async (ctx,next)=>{
-  db.addOne()
-      .then(val=>{
-        console.log(val)
-      },err=>{
-        console.log(err)
-      })
+router.get('/users',async (ctx,next)=>{
+  const model=db.usersModel()
+  model.find({},(err,data)=>{
+    if(!err){
+      console.log(data)
+      ctx.body={
+        status:'333'
+      }
+    }
+  })
+  const data=new model({
+    userName:'zzz',
+    userPassword:'222'
+  }) 
+  data.save(err=>{
+    if(err){
+      console.log(err);
+    }else{
+      console.log('tianjiachengg')
+    }
+  })
 });
+router.get('/others',async (ctx,next)=>{
+  const model=db.othersModel()
+  const data=new model({
+    userName:'zzz',
+    userPassword:'222'
+  })
+  data.save(err=>{
+    if(err){
+      console.log(err);
+    }else{
+      console.log('otherssucess')
+    }
+  })
+});
+
+
 router.get('/string', async (ctx, next) => {
   ctx.body = 'koa2 string'
 })
